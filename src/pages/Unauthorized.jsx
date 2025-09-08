636a10358b00841209972541d41647af03e509d4
AbhishekPatil1310 <ap7497493@gmail.com>
2025-09-08 19:47:08 +0530

    Delete src directory

--- src/pages/Unauthorized.jsx
+++
@@ -1,7 +0,0 @@
-export default function Unauthorized() {
-  return (
-    <h1 className="pt-24 text-center text-2xl text-red-600">
-      403 – You do not have access to this page.
-    </h1>
-  );
-}
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
-import { useDispatch } from 'react-redux';
-
-export const store = configureStore({
-  reducer: { auth: authReducer },
-});
-
-export const useAppDispatch = () => useDispatch(); // typed hook convenience
