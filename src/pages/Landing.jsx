80f944995a9f312505c07d10f4a930329eb591eb
AbhishekPatil1310 <ap7497493@gmail.com>
2025-09-09 19:39:29 +0530

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,10 @@
 import Hero from '../components/Hero';
-import About from '../components/About';
 
 export default function Landing() {
   return (
     <main className="font-sans">
       <Hero />
-      <About />
     </main>
   );
 }
+
